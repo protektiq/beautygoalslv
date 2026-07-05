14d5f740d358555d4d9bdfd13644281d25698365
protektiq <youimagineai@gmail.com>
2026-07-05 08:13:29 -0700

    completed 4.2

--- beautygoalslv/next.config.mjs
+++ beautygoalslv/next.config.mjs
@@ -1,4 +1,54 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/sofwave",
+        destination: "/services",
+        permanent: true,
+      },
+      {
+        source: "/xerf",
+        destination: "/services",
+        permanent: true,
+      },
+      {
+        source: "/aerolase-neo-elite",
+        destination: "/services",
+        permanent: true,
+      },
+      {
+        source: "/thread-lift",
+        destination: "/services",
+        permanent: true,
+      },
+      {
+        source: "/facial",
+        destination: "/services",
+        permanent: true,
+      },
+      {
+        source: "/emsculpt-neo",
+        destination: "/services",
+        permanent: true,
+      },
+      {
+        source: "/other-services",
+        destination: "/services",
+        permanent: true,
+      },
+      {
+        source: "/booking-page",
+        destination: "/contact",
+        permanent: true,
+      },
+      {
+        source: "/about-us",
+        destination: "/about",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
